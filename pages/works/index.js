2ce2d6dec9528a6fde8221bbb50a5e5430cc7716
takahirokaji <tk69kj619@gmail.com>
2021-10-04 16:21:09 +0900

    add: flexbox

--- pages/works/index.js
+++ pages/works/index.js
@@ -3,9 +3,64 @@ import styles from "../../styles/Home.module.scss";
 import AppCard from "../../components/atom/AppCard";
 import Link from "next/link";
 import Router from "next/router";
+import styled from "styled-components";
 
 export default function Home() {
   const appData = [
+    {
+      id: 1,
+      name: "パンプラージュ",
+      info: "大学内の情報を一元化することによる大学生支援サービス",
+      image: "https://source.unsplash.com/random",
+    },
+    {
+      id: 2,
+      name: "ゴミアプリ",
+      info: "ゴミの通知をしてくれるゴミのアプリ",
+      image: "https://source.unsplash.com/random",
+    },
+    {
+      id: 3,
+      name: "On-be",
+      info: "あの会話を取り戻したい！",
+      image: "https://source.unsplash.com/random",
+    },
+    {
+      id: 1,
+      name: "パンプラージュ",
+      info: "大学内の情報を一元化することによる大学生支援サービス",
+      image: "https://source.unsplash.com/random",
+    },
+    {
+      id: 2,
+      name: "ゴミアプリ",
+      info: "ゴミの通知をしてくれるゴミのアプリ",
+      image: "https://source.unsplash.com/random",
+    },
+    {
+      id: 3,
+      name: "On-be",
+      info: "あの会話を取り戻したい！",
+      image: "https://source.unsplash.com/random",
+    },
+    {
+      id: 1,
+      name: "パンプラージュ",
+      info: "大学内の情報を一元化することによる大学生支援サービス",
+      image: "https://source.unsplash.com/random",
+    },
+    {
+      id: 2,
+      name: "ゴミアプリ",
+      info: "ゴミの通知をしてくれるゴミのアプリ",
+      image: "https://source.unsplash.com/random",
+    },
+    {
+      id: 3,
+      name: "On-be",
+      info: "あの会話を取り戻したい！",
+      image: "https://source.unsplash.com/random",
+    },
     {
       id: 1,
       name: "パンプラージュ",
@@ -33,20 +88,27 @@ export default function Home() {
   };
 
   return (
-    <div className={styles.container}>
-      <main className={styles.main}>
-        {appData.map((data) => {
-          return (
-            <AppCard
-              key={data.id}
-              name={data.name}
-              info={data.info}
-              image={data.image}
-              onClickEvent={(showCard, handler)}
-            />
-          );
-        })}
-      </main>
-    </div>
+    <Flex>
+      {appData.map((data) => {
+        return (
+          <AppCard
+            key={data.id}
+            name={data.name}
+            info={data.info}
+            image={data.image}
+            onClickEvent={(showCard, handler)}
+          />
+        );
+      })}
+    </Flex>
   );
 }
+
+const Flex = styled.div`
+  display: flex;
+  align-items: center;
+  flex-direction: row;
+  flex-wrap: wrap;
+  justify-content: center;
+  padding: 1em;
+`;
